7ff34390ee991c0f338c250775ec0c045b72c518
mohit0003 <31645375+mohit0003@users.noreply.github.com>
2017-09-05 12:05:49 +0530

    tower of hanoi
    original code

---
+++ disc.py
@@ -0,0 +1,16 @@
+def hanoi(n,source,spare,target):
+    '''
+    objective:to solve problem of tower of hanoi using n discs and 3 poles
+    input parameters:n,source,spare,target:numeric value
+    return value:none
+    '''
+
+    assert n>0
+    if n==1:
+        print ('Move a disk from',source,'to',target)
+    else:
+        hanoi(n-1,source,target,spare)
+        print('Move a disk from',source,'to',target)
+        hanoi(n-1,spare,source,target)
+
+hanoi(3,1,2,3)
